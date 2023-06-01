d0fe9949d12fc89ec56e398e38a73730f3e45013
Kunjan Manandhar <200495256@student.georgianc.on.ca>
2023-06-01 12:05:58 -0400

    improved script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-# Get the fully-qualified domain name (FQDN)
-fqdn=$(hostname)
+# Get hostname and fully qualified domain name
+hostname=$(hostname)
+fqdn=$(hostname -f)
 
-# Get the operating system name and version
-os_info=$(hostnamectl | awk '/Operating System:/ {print $3,$4}')
+# Get operating system information
+os_name=$(lsb_release -ds | cut -d '"' -f 2)
+os_version=$(lsb_release -rs)
 
-# Get IP addresses excluding 127.0.0.1
-ip_addresses=$(hostname -I | tr ' ' '\n' | grep -v '^127')
+# Get default IP address
+ip_address=$(ip route get 8.8.8.8 | awk '/src/ { print $NF }')
 
-# Get the available space in the root filesystem
-space=$(df -h / | awk '/\// {print $4}')
+# Get free disk space on root filesystem
+disk_space=$(df -h --output=avail / | awk 'NR==2{print $1}')
 
-# Print the information
-echo "Fully-Qualified Domain Name (FQDN): $fqdn"
-echo "Operating System: $os_info"
-echo "IP Addresses: $ip_addresses"
-echo "Available Space in Root Filesystem: $space"
+# Output template
+output_template="
+Report for $hostname
+========================
 
+Hostname: $hostname
+Fully Qualified Domain Name: $fqdn
+
+Operating System: $os_name
+OS Version: $os_version
+
+Default IP Address: $ip_address
+
+Root Filesystem Space: $disk_space
+
+"
+
+# Display the output
+echo "$output_template"
