05456bdb282d26131acf482e39fafc13efe1ff40
Kunjan Manandhar <200495256@student.georgianc.on.ca>
2023-06-08 11:07:38 -0400

    improved script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-# Get hostname and fully qualified domain name
-hostname=$(hostname)
-fqdn=$(hostname -f)
-
-# Get operating system information
-os_name=$(lsb_release -ds | cut -d '"' -f 2)
-os_version=$(lsb_release -rs)
-
-# Get default IP address
-ip_address=$(ip route get 8.8.8.8 | awk '/src/ { print $NF }')
-
-# Get free disk space on root filesystem
-disk_space=$(df -h --output=avail / | awk 'NR==2{print $1}')
-
 # Output template
 output_template="
-Report for $hostname
-========================
-
-Hostname: $hostname
-Fully Qualified Domain Name: $fqdn
-
-Operating System: $os_name
-OS Version: $os_version
-
-Default IP Address: $ip_address
-
-Root Filesystem Space: $disk_space
-
+Report for $(hostname)
+===============
+FQDN: $(hostname --fqdn)
+Operating System name and version: $(lsb_release -d | awk -F'\t' '{print $2}')
+IP Address: $(ip route get 8.8.8.8 | awk '{print $7}')
+Root Filesystem Free Space: $(df -h / | awk 'NR==2 {print $4}')
+===============
 "
 
 # Display the output
