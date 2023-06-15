8ea9003bacd9f04791d33b6a13488bf6047ea1dc
Kunjan Manandhar <200495256@student.georgianc.on.ca>
2023-06-15 11:04:52 -0400

    Modified Script

---
+++ bash/welcome-message.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# This script produces a dynamic welcome message.
+# It should look like: "Welcome to planet hostname, title name!"
+
+# Task 1: Use the variable $USER instead of the myname variable to get your name
+name="$USER"
+
+# Task 2: Dynamically generate the value for your hostname variable using the hostname command
+hostname="$(hostname)"
+
+# Task 3: Add the time and day of the week to the welcome message
+currentTime="$(date +"%I:%M %p")"
+currentDay="$(date +"%A")"
+welcomeMessage="It is $currentDay at $currentTime."
+
+# Task 4: Set the title using the day of the week
+case "$current_day" in
+  Monday)
+    title="Master of the Week"
+    ;;
+  Tuesday)
+    title="Captain of Fortune"
+    ;;
+  Wednesday)
+    title="Sultan of Serendipity"
+    ;;
+  Thursday)
+    title="Emperor of Enthusiasm"
+    ;;
+  Friday)
+    title="Guru of Good Vibes"
+    ;;
+  Saturday)
+    title="Champion of Cheerfulness"
+    ;;
+  Sunday)
+    title="Ruler of Relaxation"
+    ;;
+  *)
+    title="Visitor of the Unknown"
+    ;;
+esac
+
+# Display the welcome message with the dynamic values
+echo "Welcome to planet $hostname, $title $name!"
+echo "$welcomeMessage"
